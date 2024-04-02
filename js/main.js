9d97183ef3a6dd6a41400862347cd33b60e29495
damiancaelen <475794@student.fontys.nl>
2024-04-02 11:05:57 +0200

    added first files

---
+++ js/main.js
@@ -0,0 +1,10 @@
+import '../css/style.css'
+
+
+document.querySelector('#app').innerHTML = `
+  <div>
+   <h1> Kraftwerk&trade; </h1>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
---
+++ readme.md
@@ -0,0 +1,10 @@
+Vite x Kraftwerk Semester 7
+
+#Why Vite?
+
+
+
+##setup
+
+
+
